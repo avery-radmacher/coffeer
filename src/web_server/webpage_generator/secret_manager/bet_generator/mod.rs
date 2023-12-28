8eca9d35ead2caf194db9159d31bb7575e20d49d
Avery Radmacher <avery.radmacher@gmail.com>
2023-12-28 16:01:05 -0500

    Refine bet generation

--- src/web_server/webpage_generator/secret_manager/bet_generator/mod.rs
+++ src/web_server/webpage_generator/secret_manager/bet_generator/mod.rs
@@ -5,14 +5,26 @@ use crate::web_server::webpage_generator::date::Date;
 
 mod random;
 
+fn generate_bet_type() -> BetType {
+    if uniform_bool() {
+        BetType::AveryOverJoshUnder
+    } else {
+        BetType::JoshOverAveryUnder
+    }
+}
+
+const EARLIEST_DATE: Date = Date(); // TODO
+const LATEST_DATE: Date = Date(); // TODO
+
+fn generate_bet_date() -> Date {
+    // TODO should be psuedo-normally set to a date in a certain time range
+    todo!()
+}
+
 pub fn generate_bet() -> Bet {
     Bet {
         bet_amount: 17500, // $175 (TODO check this is correct)
-        bet_type: if uniform_bool() {
-            BetType::AveryOverJoshUnder
-        } else {
-            BetType::JoshOverAveryUnder
-        },
-        date: Date(), // TODO should be psuedo-normally set to a date in a certain time range
+        bet_type: generate_bet_type(),
+        date: generate_bet_date(),
     }
 }
