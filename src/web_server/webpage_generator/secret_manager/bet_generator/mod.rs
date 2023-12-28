772575f3f2b63961352c765a2305ce1430776e6d
Avery Radmacher <avery.radmacher@gmail.com>
2023-12-28 15:52:04 -0500

    Add mock random functions

--- src/web_server/webpage_generator/secret_manager/bet_generator/mod.rs
+++ src/web_server/webpage_generator/secret_manager/bet_generator/mod.rs
@@ -1,3 +1,5 @@
+use self::random::uniform_bool;
+
 use super::{Bet, BetType};
 use crate::web_server::webpage_generator::date::Date;
 
@@ -5,8 +7,12 @@ mod random;
 
 pub fn generate_bet() -> Bet {
     Bet {
-        bet_amount: 17500,                     // $175 (TODO check this is correct)
-        bet_type: BetType::AveryOverJoshUnder, // TODO should be randomly set to one of two choices
+        bet_amount: 17500, // $175 (TODO check this is correct)
+        bet_type: if uniform_bool() {
+            BetType::AveryOverJoshUnder
+        } else {
+            BetType::JoshOverAveryUnder
+        },
         date: Date(), // TODO should be psuedo-normally set to a date in a certain time range
     }
 }
---
+++ src/web_server/webpage_generator/secret_manager/bet_generator/random.rs
@@ -0,0 +1,11 @@
+fn uniform(range: u32) -> u32 {
+    todo!()
+}
+
+pub fn uniform_bool() -> bool {
+    uniform(2) == 1
+}
+
+pub fn psuedo_normal(min: i32, max: i32, z_range: f64) -> i32 {
+    todo!()
+}
