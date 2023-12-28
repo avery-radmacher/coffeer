db4c998f1d8f89b37b923667e358d317eb5c51e3
Avery Radmacher <avery.radmacher@gmail.com>
2023-12-28 16:06:45 -0500

    Add date arithmetic

--- src/web_server/webpage_generator/date.rs
+++ src/web_server/webpage_generator/date.rs
@@ -1,8 +1,24 @@
+/// Represents an entire midnight-to-midnight date (year/month/day). Time zone agnostic.
 pub struct Date();
+
+/// Represents a signed duration of whole days.
+pub struct Days(pub i32);
 
 impl Date {
     /// Returns `true` iff the date represents a day in the future.
     pub fn is_future(&self) -> bool {
         true
+    }
+
+    /// Returns the days since the other date.
+    pub fn days_since(&self, other: &Self) -> Days {
+        todo!()
+    }
+}
+
+impl std::ops::Sub for Date {
+    type Output = Days;
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.days_since(&rhs)
     }
 }
