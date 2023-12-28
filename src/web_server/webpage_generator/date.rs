fed98b448d95628ef20f140145ce66a0a739b906
Avery Radmacher <avery.radmacher@gmail.com>
2023-12-28 17:09:56 -0500

    Find and use date library

--- src/web_server/webpage_generator/date.rs
+++ src/web_server/webpage_generator/date.rs
@@ -1,27 +1,34 @@
+use date_time::date_tuple::DateTuple;
+
 /// Represents an entire midnight-to-midnight date (year/month/day). Time zone agnostic.
-pub struct Date();
+pub struct Date(DateTuple);
 
 /// Represents a signed duration of whole days.
 pub struct Days(pub i32);
 
 impl Date {
     // Constructs a `Date` to represent the year-month-day triple. Panics on invalid input.
-    pub const fn from_year_month_day(year: u32, month: u32, day: u32) -> Self {
-        Date() // TODO
+    pub fn from_year_month_day(year: u32, month: u32, day: u32) -> Self {
+        Date(
+            DateTuple::new(year as u16, month as u8, day as u8)
+                .expect("Attempted to construct invalid Date"),
+        )
     }
 
     /// Returns `true` iff the date represents a day in the future.
     pub fn is_future(&self) -> bool {
-        true
+        self.0 > DateTuple::today()
     }
 
     /// Returns the days since the other date.
     pub fn days_since(&self, other: &Self) -> Days {
-        todo!()
+        Days(self.0.to_days() as i32 - other.0.to_days() as i32)
     }
 
     pub fn add_days(&self, days: &Days) -> Self {
-        todo!()
+        let mut date = self.0.clone();
+        date.add_days(days.0 as u32);
+        Date(date)
     }
 }
 
--- src/web_server/webpage_generator/secret_manager/bet_generator/mod.rs
+++ src/web_server/webpage_generator/secret_manager/bet_generator/mod.rs
@@ -11,16 +11,23 @@ fn generate_bet_type() -> BetType {
     }
 }
 
-const EARLIEST_DATE: Date = Date::from_year_month_day(2024, 3, 1); // TODO refine day
-const LATEST_DATE: Date = Date::from_year_month_day(2024, 9, 1); // TODO refine day
+// TODO make statics?
+fn earliest_date() -> Date {
+    // TODO refine day
+    Date::from_year_month_day(2024, 3, 1)
+}
+fn latest_date() -> Date {
+    // TODO refine day
+    Date::from_year_month_day(2024, 9, 1)
+}
 
 fn generate_bet_date() -> Date {
-    let Days(min) = EARLIEST_DATE - EARLIEST_DATE;
-    let Days(max) = LATEST_DATE - EARLIEST_DATE;
+    let Days(min) = earliest_date() - earliest_date();
+    let Days(max) = latest_date() - earliest_date();
 
     let days_after_earliest_date = Days(random::psuedo_normal(min, max, 3.0));
 
-    EARLIEST_DATE.add_days(&days_after_earliest_date)
+    earliest_date().add_days(&days_after_earliest_date)
 }
 
 pub fn generate_bet() -> Bet {
