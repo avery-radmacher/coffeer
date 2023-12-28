6d355513bb438c9c679a1a48ce617d18ddb6d9aa
Avery Radmacher <avery.radmacher@gmail.com>
2023-12-28 15:35:13 -0500

    Add bet and mock secret generation

--- src/web_server/webpage_generator/secret_manager/mod.rs
+++ src/web_server/webpage_generator/secret_manager/mod.rs
@@ -1,2 +1,28 @@
+use super::date::Date;
+
 mod io;
 mod secret_generator;
+
+pub enum BetType {
+    /// Josh wins if the balance is over the bet amount; Avery wins if the balance is below the bet amount.
+    JoshOverAveryUnder,
+    /// Avery wins if the balance is over the bet amount; Josh wins if the balance is below the bet amount.
+    AveryOverJoshUnder,
+}
+
+pub struct Bet {
+    /// The amount (in cents) to compare against the balance.
+    pub bet_amount: i32,
+    /// The type of bet being made.
+    pub bet_type: BetType,
+    /// The date upon which the bet takes effect.
+    pub date: Date,
+}
+
+pub fn get_or_create_bet() -> Bet {
+    Bet {
+        bet_amount: 17500,                     // $175 (TODO check this is correct)
+        bet_type: BetType::AveryOverJoshUnder, // TODO should be randomly set to one of two choices
+        date: Date(), // TODO should be psuedo-normally set to a date in a certain time range
+    }
+}
