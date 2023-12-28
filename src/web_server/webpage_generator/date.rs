0aed439e808839b0668e1e8e99c78f381a8bd38a
Avery Radmacher <avery.radmacher@gmail.com>
2023-12-28 15:35:30 -0500

    Enhance webpage generation

--- src/web_server/webpage_generator/date.rs
+++ src/web_server/webpage_generator/date.rs
@@ -1,1 +1,8 @@
 pub struct Date();
+
+impl Date {
+    /// Returns `true` iff the date represents a day in the future.
+    pub fn is_future(&self) -> bool {
+        true
+    }
+}
--- src/web_server/webpage_generator/mod.rs
+++ src/web_server/webpage_generator/mod.rs
@@ -3,7 +3,12 @@ mod secret_manager;
 
 pub fn generate_webpage() -> String {
     // get the secret
-    // if the secret is ready to be divulged, generate that webpage
-    // else, print a generic "not ready" webpage
-    "(mock) Webpage HTML".to_owned()
+    let bet = secret_manager::get_or_create_bet();
+    if bet.date.is_future() {
+        // if the secret is ready to be divulged, generate that webpage
+        "(mock) HTML of divulged secret".to_owned()
+    } else {
+        // else, print a generic "not ready" webpage
+        "(mock) \"Not Ready\" HTML".to_owned()
+    }
 }
