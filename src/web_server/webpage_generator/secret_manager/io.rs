d8a661618dd0eb159fc7880368f39beb65164d2e
Avery Radmacher <avery.radmacher@gmail.com>
2023-12-29 11:48:48 -0500

    Add serde_json and implement io

--- src/web_server/webpage_generator/secret_manager/io.rs
+++ src/web_server/webpage_generator/secret_manager/io.rs
@@ -1,13 +1,29 @@
 use super::Bet;
+use std::fs::{File, OpenOptions};
+use std::path::PathBuf;
+use std::str::FromStr;
 
-fn get_path() -> String {
-    "/data".to_owned()
+fn get_path() -> PathBuf {
+    PathBuf::from_str(".\\data\\bet.json").expect("Error creating path")
 }
 
 pub fn try_get_secret() -> Option<Bet> {
-    None // TODO
+    let path = get_path();
+    if !path.exists() {
+        return None;
+    }
+
+    let file = File::open(path).expect("Error opening file");
+    let bet = serde_json::from_reader(file).expect("Error reading file");
+    Some(bet)
 }
 
 pub fn store_secret(bet: &Bet) {
-    todo!()
+    let file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(get_path())
+        .expect("Error creating new file");
+
+    serde_json::to_writer_pretty(file, bet).expect("Error writing secret");
 }
