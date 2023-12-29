bc0c923a1ffeb48aff672c62ae4db2815af5fcf8
Avery Radmacher <avery.radmacher@gmail.com>
2023-12-29 11:46:37 -0500

    Fix logic

--- src/web_server/webpage_generator/mod.rs
+++ src/web_server/webpage_generator/mod.rs
@@ -4,7 +4,7 @@ mod secret_manager;
 pub fn generate_webpage() -> String {
     // get the secret
     let bet = secret_manager::get_or_create_bet();
-    if bet.date.is_future() {
+    if !bet.date.is_future() {
         // if the secret is ready to be divulged, generate that webpage
         "(mock) HTML of divulged secret".to_owned()
     } else {
