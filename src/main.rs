9e79cb9f01d0ba07fb7b7ec1dbbc819cfe331885
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-15 13:16:51 -0500

    Test that warp and tokio are working
    listing copied from https://crates.io/crates/warp/0.3.6

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use warp::Filter;
+
 mod web_server;
 
-fn main() {
-    if let Err(msg) = web_server::run() {
-        eprintln!("Error: {}", msg);
-    }
+// fn main() {
+//     if let Err(msg) = web_server::run() {
+//         eprintln!("Error: {}", msg);
+//     }
+// }
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
