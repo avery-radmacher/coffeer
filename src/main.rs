04aae31927b2f68e1c6e530864ee14d54814414b
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-15 14:05:44 -0500

    Remove unused

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,6 @@ use warp::Filter;
 
 mod webpage_generator;
 
-// fn main() {
-//     if let Err(msg) = web_server::run() {
-//         eprintln!("Error: {}", msg);
-//     }
-// }
-
 #[tokio::main]
 async fn main() {
     let coffee = warp::path!("coffee").map(webpage_generator::generate_webpage);
