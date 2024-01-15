7263448064e1a0cf023ddf47db48a3be667e290f
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-15 14:05:14 -0500

    Add bet revelation

--- src/webpage_generator/mod.rs
+++ src/webpage_generator/mod.rs
@@ -6,7 +6,7 @@ pub fn generate_webpage() -> String {
     let bet = secret_manager::get_or_create_bet();
     if !bet.date.is_future() {
         // if the secret is ready to be divulged, generate that webpage
-        "(mock) HTML of divulged secret".to_owned()
+        bet.reveal()
     } else {
         // else, print a generic "not ready" webpage
         "The bet has not yet reached maturity.".to_owned()
--- src/webpage_generator/secret_manager/mod.rs
+++ src/webpage_generator/secret_manager/mod.rs
@@ -22,6 +22,34 @@ pub struct Bet {
     pub date: Date,
 }
 
+impl Bet {
+    pub fn reveal(&self) -> String {
+        let amount_dollars = format!("${}", self.bet_amount / 100);
+        let cents = self.bet_amount % 100;
+        let amount_cents = if cents != 0 {
+            format!(".{:02}", cents)
+        } else {
+            "".into()
+        };
+
+        let amount = format!("The bet amount was {amount_dollars}{amount_cents}.");
+
+        let (over_winner, under_winner) = match self.bet_type {
+            BetType::AveryOverJoshUnder => ("Avery", "Josh"),
+            BetType::JoshOverAveryUnder => ("Josh", "Avery"),
+        };
+
+        let outcome = format!("{over_winner} wins if the remaining balance is over this amount; {under_winner} wins if the remaining balance is below this amount.");
+
+        let maturity = format!(
+            "The bet reached maturity on {}.",
+            self.date.to_readable_string()
+        );
+
+        return format!("{maturity}\n{amount}\n{outcome}");
+    }
+}
+
 pub fn get_or_create_bet() -> Bet {
     if let Some(secret) = io::try_get_secret() {
         secret
