180398de6622d72d651200f75af1b2b5a7280471
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-16 18:50:13 -0500

    Serve static files
    test.html inspired by https://docs.rs/warp/0.3.6/warp/reply/fn.html.html

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ mod webpage_generator;
 
 #[tokio::main]
 async fn main() {
-    let coffee = warp::path!("coffee").map(webpage_generator::generate_webpage);
+    let coffee = warp::path!("coffee").map(webpage_generator::generate_webpage); // TODO refactor into api calls
 
-    warp::serve(coffee).run(([127, 0, 0, 1], 3030)).await;
+    let static_files = warp::fs::dir("./www/static"); // TODO assume requests without file extensions are html requests
+
+    let all_routes = static_files.or(coffee);
+
+    warp::serve(all_routes).run(([127, 0, 0, 1], 3030)).await;
 }
