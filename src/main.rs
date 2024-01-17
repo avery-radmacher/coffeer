5ce64b0d39f9b2e0e8082ee093bcf49bb448a772
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-17 11:42:59 -0500

    Change port number

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,5 @@ async fn main() {
 
     let all_routes = api.or(static_files);
 
-    warp::serve(all_routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(all_routes).run(([127, 0, 0, 1], 3000)).await;
 }
