fa43bde1899fccc6af2dd91bf94000bef18b50b5
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-16 19:00:51 -0500

    Add API support

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ mod webpage_generator;
 
 #[tokio::main]
 async fn main() {
-    let coffee = warp::path!("coffee").map(webpage_generator::generate_webpage); // TODO refactor into api calls
+    let api = warp::path("api")
+        .and(warp::path("coffee"))
+        .map(webpage_generator::generate_webpage); // TODO refactor into api calls
+                                                   // TODO handle failures instead of falling back to looking up /api in the filesystem
 
     let static_files = warp::fs::dir("./www/static"); // TODO assume requests without file extensions are html requests
 
-    let all_routes = static_files.or(coffee);
+    let all_routes = api.or(static_files);
 
     warp::serve(all_routes).run(([127, 0, 0, 1], 3030)).await;
 }
