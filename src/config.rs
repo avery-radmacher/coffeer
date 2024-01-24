6cdd04579171d339a1201a56be1a8a428ebdd9b3
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-24 16:37:07 -0500

    Add config reading

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,7 @@
+use crate::io;
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::str::FromStr;
+use std::{net::SocketAddr, path::PathBuf};
 
 /// Represents app-level configuration.
 #[derive(Serialize, Deserialize)]
@@ -8,4 +10,13 @@ pub struct AppConfig {
     listen_on: SocketAddr,
     /// The parties to the bet.
     parties: (String, String),
+}
+
+pub fn try_get_app_config() -> Option<AppConfig> {
+    let path = PathBuf::from_str(".\\data\\config.json").expect("Error creating path");
+    if !path.exists() {
+        return None;
+    }
+
+    Some(io::read_json_file(&path))
 }
