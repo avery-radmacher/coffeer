f29aec62a712bf1d5225d080f0ea2dd4e70f98bf
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-24 16:03:51 -0500

    Decompose file reading

--- src/webpage_generator/io.rs
+++ src/webpage_generator/io.rs
@@ -1,4 +1,5 @@
 use crate::webpage_generator::secret_manager::Bet;
+use serde::de::DeserializeOwned;
 use std::fs::{self, File, OpenOptions};
 use std::path::PathBuf;
 use std::str::FromStr;
@@ -7,15 +8,22 @@ fn get_path() -> PathBuf {
     PathBuf::from_str(".\\data\\bet.json").expect("Error creating path")
 }
 
+fn read_json_file<T>(path: &PathBuf) -> T
+where
+    T: DeserializeOwned,
+{
+    let file = File::open(path).expect("Error opening file");
+    let item: T = serde_json::from_reader(file).expect("Error reading file");
+    item
+}
+
 pub fn try_get_secret() -> Option<Bet> {
     let path = get_path();
     if !path.exists() {
         return None;
     }
 
-    let file = File::open(path).expect("Error opening file");
-    let bet = serde_json::from_reader(file).expect("Error reading file");
-    Some(bet)
+    Some(read_json_file(&path))
 }
 
 pub fn store_secret(bet: &Bet) {
