f6791ac4c98d183b6ef13c8e4c9b1dd3cc4855b5
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-24 16:08:45 -0500

    Decompose file writing

--- src/webpage_generator/io.rs
+++ src/webpage_generator/io.rs
@@ -1,5 +1,6 @@
 use crate::webpage_generator::secret_manager::Bet;
 use serde::de::DeserializeOwned;
+use serde::ser::Serialize;
 use std::fs::{self, File, OpenOptions};
 use std::path::PathBuf;
 use std::str::FromStr;
@@ -8,13 +9,23 @@ fn get_path() -> PathBuf {
     PathBuf::from_str(".\\data\\bet.json").expect("Error creating path")
 }
 
-fn read_json_file<T>(path: &PathBuf) -> T
-where
-    T: DeserializeOwned,
-{
+fn read_json_file<T: DeserializeOwned>(path: &PathBuf) -> T {
     let file = File::open(path).expect("Error opening file");
     let item: T = serde_json::from_reader(file).expect("Error reading file");
     item
+}
+
+fn write_json_file<T: Serialize>(path: &PathBuf, value: &T) {
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(parent).expect("Error creating storage directory");
+    }
+    let file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(path)
+        .expect("Error creating new file");
+
+    serde_json::to_writer_pretty(file, value).expect("Error writing file");
 }
 
 pub fn try_get_secret() -> Option<Bet> {
@@ -27,15 +38,5 @@ pub fn try_get_secret() -> Option<Bet> {
 }
 
 pub fn store_secret(bet: &Bet) {
-    let path = get_path();
-    if let Some(parent) = path.parent() {
-        fs::create_dir_all(parent).expect("Error creating storage directory");
-    }
-    let file = OpenOptions::new()
-        .write(true)
-        .create_new(true)
-        .open(path)
-        .expect("Error creating new file");
-
-    serde_json::to_writer_pretty(file, bet).expect("Error writing secret");
+    write_json_file(&get_path(), bet)
 }
