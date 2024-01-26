651a2acbdc029d3f6bd062d62f1adcbc78421fb2
Avery Radmacher <avery.radmacher@gmail.com>
2024-01-26 10:28:50 -0500

    Add balance API

--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -9,7 +9,11 @@ pub struct ApiConfig {
 
 pub fn api(config: ApiConfig) -> BoxedFilter<(impl Reply,)> {
     warp::path("api")
-        .and(coffee::coffee(config.party_1, config.party_2).or(util::not_found_404()))
+        .and(
+            coffee::coffee(config.party_1, config.party_2)
+                .or(balance::balance())
+                .or(util::not_found_404()),
+        )
         .boxed()
 }
 
@@ -20,5 +24,49 @@ mod util {
         warp::any()
             .map(|| warp::http::StatusCode::NOT_FOUND)
             .boxed()
+    }
+}
+
+mod balance {
+    use crate::io;
+    use std::path::PathBuf;
+    use warp::{filters::BoxedFilter, Filter, Reply};
+
+    fn get_path() -> PathBuf {
+        io::storage_directory().join("balance.json")
+    }
+
+    fn get_balance() -> i32 {
+        let balance = io::try_read_json_file(&get_path());
+        if let Some(balance) = balance {
+            return balance;
+        }
+        set_balance(0);
+        return 0;
+    }
+
+    fn set_balance(balance: i32) {
+        io::write_json_file(&get_path(), &balance);
+    }
+
+    fn get() -> BoxedFilter<(impl Reply,)> {
+        warp::get()
+            .map(|| warp::reply::json(&get_balance()))
+            .boxed()
+    }
+
+    fn put() -> BoxedFilter<(impl Reply,)> {
+        // TODO auth
+        warp::put()
+            .and(warp::body::json())
+            .map(|balance: i32| {
+                set_balance(balance);
+                warp::reply()
+            })
+            .boxed()
+    }
+
+    pub fn balance() -> BoxedFilter<(impl Reply,)> {
+        warp::path!("balance").and(get().or(put())).boxed()
     }
 }
